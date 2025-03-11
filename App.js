3eafd2d065af60195bbc2e9f639b67bf7f01a533
OtavioMunhhoz <otavio2k20@gmail.com>
2025-03-10 22:48:25 -0300

    aula exemplo1

--- App.js
+++ App.js
@@ -3,11 +3,16 @@ import { StyleSheet, Text, View } from 'react-native';
 
 import Constants from 'expo-constants';
 
+// Exemplos 
+import Exemplo01 from './src/exemplo/exemplo-01';
+
+//Atividades
+
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Forro Boys!</Text>
-      <StatusBar style="auto"/>
+      <Exemplo01/>
+      <StatusBar style="light"/>
     </View>
   );
 }
@@ -15,9 +20,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'orange',
-    alignItems: 'center',
-    justifyContent: 'center',
-    marginTop: Constants.statusBarHeight,
+    backgroundColor: 'pink',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+    //marginTop: Constants.statusBarHeight,
+    padding:8,
+    paddingBlock: Constants.statusBarHeight,
   },
 });
